621d32824177df019c25b04bb964765eb0f4b7e2
EdwardXie <EdwardXie@DMPCPF0SV915>
2019-02-28 16:50:55 -0500

    Put noexcept on things

--- ThreadPool/thread_pool.h
+++ ThreadPool/thread_pool.h
@@ -142,7 +142,7 @@ namespace exlib {
 			/*
 				Makes threads look for tasks. Useful after stop() has been called to reactivate job search.
 			*/
-			void reactivate()
+			void reactivate() noexcept
 			{
 				_active=true;
 				_signal_start.notify_all();
@@ -159,7 +159,7 @@ namespace exlib {
 			/*
 				Whether the threads should be running. Not a guarantee that threads are or are not running.
 			*/
-			bool running() const
+			bool running() const noexcept
 			{
 				return _running;
 			}
@@ -167,7 +167,7 @@ namespace exlib {
 			/*
 				Whether the threads are looking for tasks.
 			*/
-			bool active() const
+			bool active() const noexcept
 			{
 				return _active;
 			}
@@ -507,7 +507,7 @@ namespace exlib {
 		/*
 			Clears the jobs handled by this threadpool without synchronization.
 		*/
-		void clear_no_sync()
+		void clear_no_sync() noexcept
 		{
 			this->_jobs.clear();
 		}
@@ -542,7 +542,7 @@ namespace exlib {
 			});
 			return count;
 		}
-		bool wait_func()
+		bool wait_func() noexcept
 		{
 			return !this->_active||this->_jobs.empty();
 		}
